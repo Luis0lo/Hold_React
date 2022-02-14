3070727cc600679859b12c0580731a7b92902992
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-02-14 20:32:01 +0000

    set for deployment

--- src/Components/RandomQuote/index.js
+++ src/Components/RandomQuote/index.js
@@ -4,14 +4,14 @@ import QuoteViewer from './QuoteViewer';
 const RandomQuote = ({ API_URL }) => {
   const [randQuote, setRandQuote] = useState('');
 
-  async function getRandQuote() {
-    const response = await fetch(`${API_URL}/quotes/random`);
-    const data = await response.json();
-    setRandQuote(data.payload[0]);
-  }
   useEffect(() => {
+    async function getRandQuote() {
+      const response = await fetch(`${API_URL}/quotes/random`);
+      const data = await response.json();
+      setRandQuote(data.payload[0]);
+    }
     getRandQuote();
-  }, []);
+  }, [API_URL]);
 
   return (
     <div>
