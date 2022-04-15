6e4a0caa7dc1e58defda0613a925c307b71df407
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-04-15 18:57:21 +0100

    test to updateHoldings func

--- src/helper.test.js
+++ src/helper.test.js
@@ -1,7 +1,7 @@
-import { twoDecimalPlacesPrice } from './helper';
+import { twoDecimalPlacesPrice, updateHoldings } from './helper';
 
 describe('when updating the shares price', () => {
-  it(`should return a num with 2 decimal max and rounded down`, () => {
+  it(`should return price with 2 decimal max and rounded down`, () => {
     //arrange //act //assert
     const price = 350.737;
     const actual = twoDecimalPlacesPrice(price);
@@ -13,7 +13,7 @@ describe('when updating the shares price', () => {
   // https://medium.com/@afolabiwaheed/how-to-test-a-function-thats-expected-to-throw-error-in-jest-2419cc7c6462
   // https://jestjs.io/docs/expect#tothrowerror
 
-  it('should throw an error if input is not a number', () => {
+  it('should throw an error if price is not a number', () => {
     expect(() => {
       twoDecimalPlacesPrice('a');
     }).toThrowError('it must be a number');
@@ -22,5 +22,55 @@ describe('when updating the shares price', () => {
     //   twoDecimalPlacesPrice('a');
     //   }
     // expect(testing).toThrowError('it must be a number');
+  });
+
+  it('should update holdings with the correct data structure', () => {
+    const holdings = [
+      {
+        name: 'TSLA',
+        quantity: 4,
+        price: 725,
+        total: 2900,
+        currentMarketValueTotal: 0,
+        currency: 'USD',
+      },
+      {
+        name: 'GOOGL',
+        quantity: 2,
+        price: 2500,
+        total: 5000,
+        currentMarketValueTotal: 0,
+        currency: 'USD',
+      },
+    ]
+
+    const data = [
+      {
+        name: 'TSLA',
+        quantity: 2,
+        price: 700,
+        total: 1400,
+        currentMarketValueTotal: 0,
+        currency: 'USD',
+      },
+      {
+        name: 'GOOGL',
+        quantity: 2,
+        price: 2500,
+        total: 5000,
+        currentMarketValueTotal: 0,
+        currency: 'USD',
+      },
+    ]
+    const input = {
+      currency: 'USD',
+      name: 'TSLA',
+      price: 750,
+      quantity: 2,
+    };
+    const index = 0;
+    //act //assert
+    const actual = updateHoldings(data, index, input)
+    expect(actual).toEqual(holdings);
   });
 });
