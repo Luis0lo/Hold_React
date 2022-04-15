3d5fbcdb74cfc4b7ac07abc58339e75c8256c205
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-04-15 11:46:24 +0100

    notes about testing

--- src/helper.test.js
+++ src/helper.test.js
@@ -10,11 +10,17 @@ describe('when updating the shares price', () => {
     expect(actual).toBe(expected);
   });
 
-//   https://medium.com/@afolabiwaheed/how-to-test-a-function-thats-expected-to-throw-error-in-jest-2419cc7c6462
+  // https://medium.com/@afolabiwaheed/how-to-test-a-function-thats-expected-to-throw-error-in-jest-2419cc7c6462
+  // https://jestjs.io/docs/expect#tothrowerror
 
   it('should throw an error if input is not a number', () => {
     expect(() => {
       twoDecimalPlacesPrice('a');
     }).toThrowError('it must be a number');
+    // option 2
+    // function testing() {
+    //   twoDecimalPlacesPrice('a');
+    //   }
+    // expect(testing).toThrowError('it must be a number');
   });
 });
