0ae89eac7bdfff628efa3d1852d6b19d8664b598
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-05-06 08:51:58 +0100

    style error page being responsive

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -1,17 +1,41 @@
 import React from 'react'
+import {
+  Stack,
+  Flex,
+  Text,
+  VStack,
+  useBreakpointValue,
+} from '@chakra-ui/react';
 
 const ErrorMessage = () => {
     return (
-        <div
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '80vh',
-      }}
-    >
-      <p style={{ fontSize: '5rem' }}>🤷‍♀️ 404 🤷‍♂️</p>
-    </div>
+      <Flex
+      w={'full'}
+      h={'90vh'}
+      backgroundImage={
+        'url(https://images.pexels.com/photos/9752173/pexels-photo-9752173.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)'
+      }
+      backgroundSize={'cover'}
+      backgroundPosition={'center center'}>
+      <VStack
+        w={'full'}
+        justify={'center'}
+        px={useBreakpointValue({ base: 4, md: 8 })}
+        bgGradient={'linear(to-r, blackAlpha.600, transparent)'}>
+        <Stack maxW={'2xl'} >
+          <Text 
+            bg='blackAlpha.700' 
+            px={5}
+            borderRadius='10'
+            color={'white'}
+            fontWeight={700}
+            lineHeight={1.5}
+            fontSize={useBreakpointValue({ base: '3xl', md: '4xl' })}>
+            Page not Found! 
+          </Text>
+        </Stack>
+      </VStack>
+      </Flex>
     )
 }
 
