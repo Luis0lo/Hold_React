e5ac301d5cf0b0854c86fce82f7a1f0e244e8de9
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-05-09 08:58:11 +0100

    fetching to weather api

--- src/Components/Weather/LocationInput/locationInput.js
+++ src/Components/Weather/LocationInput/locationInput.js
@@ -2,10 +2,10 @@ import css from './locationInput.module.css';
 import { BsSearch } from 'react-icons/bs';
 
 const LocationInput = (props) => {
+
   return (
     <div className={css.locationContainer}>
       <input
-        value={props.location}
         onChange={(event) => props.setLocation(event.target.value)}
         onKeyPress={props.searchLocation}
         placeholder="Enter Location"
--- src/Pages/Weather.js
+++ src/Pages/Weather.js
@@ -2,13 +2,37 @@ import { useState } from 'react';
 import LocationInput from '../Components/Weather/LocationInput/locationInput';
 
 const Weather = () => {
-  const [location, setLocation] = useState('');
+  const [location, setLocation] = useState(null);
+  const [weather, setWeather] = useState([]);
   console.log(location);
 
-  const searchLocation = () => {
-    console.log(location);
-  };
+  const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${weatherApiKey}`;
 
+  const searchLocation = async (event) => {
+    if (event.key === 'Enter' || event.type === 'click') {
+      try {
+        const response1 = await fetch(url);
+        const currentDay = await response1.json();
+        console.log(location, currentDay);
+        const response2 = await fetch(
+          `https://api.openweathermap.org/data/2.5/onecall?lat=${currentDay.coord.lat}&lon=${currentDay.coord.lon}&exclude=minutely&units=metric&appid=${weatherApiKey}`
+        );
+        const weekWeather = await response2.json();
+        console.log(weekWeather);
+        setWeather({
+          city: currentDay.name,
+          country: currentDay.sys.country,
+          id: currentDay.sys.id,
+        });
+      } catch (err) {
+        console.log(err.message);
+      }
+    } else {
+      console.log('nothing yet');
+    }
+  };
+  console.log(weather);
   return (
     <div
       style={{
