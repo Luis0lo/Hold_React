97497b6531226deedfae03145d48a9d79e7a3ee7
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-05-10 16:36:18 +0100

    set weather data

--- src/Pages/Weather.js
+++ src/Pages/Weather.js
@@ -1,9 +1,12 @@
 import { useState } from 'react';
 import LocationInput from '../Components/Weather/LocationInput/locationInput';
+import moment from "moment";
 
 const Weather = () => {
   const [location, setLocation] = useState(null);
+  const [locationDetails, setlocationDetails] = useState([]);
   const [weather, setWeather] = useState([]);
+  let weatherInfo = [];
   console.log(location);
 
   const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY;
@@ -18,13 +21,35 @@ const Weather = () => {
         const response2 = await fetch(
           `https://api.openweathermap.org/data/2.5/onecall?lat=${currentDay.coord.lat}&lon=${currentDay.coord.lon}&exclude=minutely&units=metric&appid=${weatherApiKey}`
         );
-        const weekWeather = await response2.json();
-        console.log(weekWeather);
-        setWeather({
+        const { daily } = await response2.json();
+        setlocationDetails({
           city: currentDay.name,
           country: currentDay.sys.country,
           id: currentDay.sys.id,
         });
+
+        daily.forEach((day, i) => {
+          weatherInfo = [
+            ...weatherInfo,
+            {
+              windSpeed: day.wind_speed + ' m/s',
+              month: moment.unix(daily[i].dt).format('MMMM'),
+              day: moment.unix(daily[i].dt).format('Do'),
+              weekDay: moment.unix(daily[i].dt).format('dddd'),
+              feelsLikeAvg: Math.round(day.feels_like.day) + ' °C',
+              humidity: Math.round(day.humidity) + ' %',
+              sunrise: moment.unix(day.sunrise).format("H:mm a"),
+              sunset: moment.unix(day.sunset).format("H:mm a"),
+              tempAver: Math.round(day.temp.day) + ' °C',
+              tempMax: Math.round(day.temp.max) + ' °C',
+              tempMin: Math.round(day.temp.min) + ' °C',
+              weather: day.weather[0].main,
+              weatherDescription: day.weather[0].description,
+              icon: day.weather[0].icon,
+            },
+          ];
+        });
+        console.log(weatherInfo);
       } catch (err) {
         console.log(err.message);
       }
@@ -32,7 +57,7 @@ const Weather = () => {
       console.log('nothing yet');
     }
   };
-  console.log(weather);
+  console.log(locationDetails);
   return (
     <div
       style={{
