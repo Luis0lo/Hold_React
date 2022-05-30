34c0180e329f2ae42385113ebe7adbc1c2bc518d
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-05-30 10:44:11 +0100

    mobile charts added

--- src/Components/Weather/Chart/hourlyChart.js
+++ src/Components/Weather/Chart/hourlyChart.js
@@ -1,4 +1,8 @@
 import {
+  
+  ComposedChart,
+  
+  Legend,
   AreaChart,
   Area,
   XAxis,
@@ -13,49 +17,81 @@ const HourlyChart = ({ hourlyWeather, data }) => {
   return (
     <div className={css.hourlyChartContainer}>
       <p>
-        Next 24h Hours Forecast{' '}
         <i>
           <b>fells</b>
-        </i>
+        </i>{' '}
+        24h Hours Forecast
       </p>
-      <ResponsiveContainer width="100%" height="100%" aspect={3}>
-        <AreaChart
-          width={500}
-          height={400}
-          data={data}
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}
-        >
-          <defs>
-            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#FF3F00" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-            </linearGradient>
-          </defs>
-          <CartesianGrid strokeDasharray="1" vertical="" horizontal="true" />
-          <XAxis dataKey="hour" />
-          <YAxis />
-          <Tooltip />
-          <Area
-            type="monotone"
-            dataKey="feelsLike"
-            stroke="#8884d8"
-            fillOpacity={1}
-            fill="url(#colorUv)"
-          />
+      <div className={css.desktopHourChart}>
+        <ResponsiveContainer width="100%" height="100%" aspect={2.5}>
+          <AreaChart
+            width={500}
+            height={400}
+            data={data}
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <defs>
+              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#FF3F00" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <CartesianGrid strokeDasharray="1" vertical="" horizontal="true" />
+            <XAxis dataKey="hour" />
+            <YAxis />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="feelsLike"
+              stroke="#8884d8"
+              fillOpacity={1}
+              fill="url(#colorUv)"
+            />
 
-          {/* <Area
+            {/* <Area
             type="monotone"
             dataKey="feelsLike"
             stroke="#FF3F00"
             fill="#8884d8"
           /> */}
-        </AreaChart>
-      </ResponsiveContainer>
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+      <div className={css.mobileHourChart}>
+        <ResponsiveContainer width="100%" height="100%" aspect={1}>
+          <ComposedChart
+            layout="vertical"
+            width={500}
+            height={500}
+            data={data}
+            margin={{
+              top: 20,
+              right: 20,
+              bottom: 20,
+              left: 20,
+            }}
+          >
+            <CartesianGrid
+              stroke="black"
+              strokeDasharray="1"
+              vertical="true"
+              horizontal=""
+            />
+            <XAxis type="number" />
+            <YAxis dataKey="hour" type="category" />
+            <Tooltip />
+            <Legend />
+            <Area dataKey="feelsLike" fill="#8884d8" stroke="#ff7300" />
+            {/* <Bar dataKey="feelsLike" barSize={20} fill="#413ea0" /> */}
+            {/* <Line dataKey="feelsLike" stroke="#ff7300" /> */}
+          </ComposedChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
--- src/Components/Weather/Chart/weatherChart.js
+++ src/Components/Weather/Chart/weatherChart.js
@@ -8,7 +8,7 @@ import {
   Legend,
   ResponsiveContainer,
 } from 'recharts';
-import css from './weatherChart.module.css'
+import css from './weatherChart.module.css';
 
 const WeatherChart = ({ weather }) => {
   const weatherChartInfo = weather.map((day) => {
@@ -19,47 +19,80 @@ const WeatherChart = ({ weather }) => {
 
   return (
     <div className={css.chartContainer}>
-    <p>8 Days Weather Forecast - Celsius</p>
-      <ResponsiveContainer width="100%" aspect={3}>
-        <LineChart
-          width={500}
-          height={300}
-          data={weatherChartInfo}
-          margin={{
-            top: 5,
-            right: 30,
-            // left: 20,
-            // bottom: 5,
-          }}
-        >
-          <CartesianGrid vertical="" horizontal="true" />
-          <XAxis dataKey="day" />
-          <YAxis />
-          <Tooltip
-            contentStyle={{
-              backgroundColor: '#F0FFFF',
-              borderRadius: '1rem',
-              border: '2px solid #5230EA',
-              padding: '0.2rem 1rem',
+      <p>8 Days Weather Forecast - Celsius</p>
+      <div className={css.desktopWeekChart}>
+        <ResponsiveContainer width="100%" aspect={3}>
+          <LineChart
+            width={500}
+            height={300}
+            data={weatherChartInfo}
+            margin={{
+              top: 5,
+              right: 30,
+              // left: 20,
+              // bottom: 5,
             }}
-          />
-          <Legend />
-          <Line
-            type="monotone"
-            dataKey="max"
-            stroke="#FF3F00"
-            activeDot={{ r: 8 }}
-            strokeWidth={2}
-          />
+          >
+            <CartesianGrid vertical="" horizontal="true" />
+            <XAxis dataKey="day" />
+            <YAxis />
+            <Tooltip
+              contentStyle={{
+                backgroundColor: '#F0FFFF',
+                borderRadius: '1rem',
+                border: '2px solid #5230EA',
+                padding: '0.2rem 1rem',
+              }}
+            />
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey="max"
+              stroke="#FF3F00"
+              activeDot={{ r: 8 }}
+              strokeWidth={2}
+            />
 
-          <Line
-            type="monotone"
-            dataKey="min"
-            stroke="#0000A6"
-            strokeWidth={2}
-          />
-        </LineChart>
-      </ResponsiveContainer>
+            <Line
+              type="monotone"
+              dataKey="min"
+              stroke="#0000A6"
+              strokeWidth={2}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+      <div className={css.mobileWeekChart}>
+        <ResponsiveContainer width="100%" height="100%" aspect={1}>
+          <LineChart
+            layout="vertical"
+            width={500}
+            height={300}
+            data={weatherChartInfo}
+            margin={{
+              top: 20,
+              right: 15,
+              left: 30,
+              // bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="1" />
+            <XAxis type="number" />
+            <YAxis dataKey="day" type="category" />
+            <Tooltip
+              contentStyle={{
+                backgroundColor: '#F0FFFF',
+                borderRadius: '1rem',
+                border: '2px solid #5230EA',
+                padding: '0.2rem 1rem',
+              }}
+            />
+            <Legend />
+            <Line dataKey="max" stroke="#FF3F00" />
+            <Line dataKey="min" stroke="#0000A6" />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
