c89cbe5d0bda565c6ea464e6c33d1bd5c09448b1
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-05-31 19:39:23 +0100

    hot fix using useCallback hook

--- src/Components/Weather/HistoricWeather/historicweather.js
+++ src/Components/Weather/HistoricWeather/historicweather.js
@@ -1,10 +1,10 @@
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
 
 const HistoricWeather = () => {
   const historicApiKey = process.env.REACT_APP_HISTORIC_WEATHER_API;
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/caracas/1989-08-28/1989-08-28?unitGroup=metric&include=days&key=${historicApiKey}&contentType=json`;
 
-  const getHistoricData = async () => {
+  const getHistoricData = useCallback(async () => {
     try {
       const response = await fetch(url);
       const data = await response.json();
@@ -12,11 +12,11 @@ const HistoricWeather = () => {
     } catch (err) {
       console.log(err.message);
     }
-  };
+  }, [url]);
 
   useEffect(() => {
     getHistoricData();
-  }, []);
+  }, [getHistoricData]);
 
   return <div></div>;
 };
