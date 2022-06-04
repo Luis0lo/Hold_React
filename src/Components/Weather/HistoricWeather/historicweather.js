2c7b49028f54b45247c192770339c26627c14d63
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-06-04 17:52:57 +0100

    conditional render, improve styling

--- src/Components/Weather/HistoricWeather/historicweather.js
+++ src/Components/Weather/HistoricWeather/historicweather.js
@@ -1,23 +1,21 @@
 import { useState, useEffect, useCallback } from 'react';
 import CalendarInput from './CalendarInput/calendarInput';
 import DisplayBirthWeather from './displayBirthWeather';
-import css from './historicWeather.module.css'
-
+import css from './historicWeather.module.css';
 
 const HistoricWeather = () => {
   const [birthdayInfo, setBirthdayInfo] = useState(null);
-  const [birthdayWeatherData, setBirthdayWeatherData] = useState(null)
+  const [birthdayWeatherData, setBirthdayWeatherData] = useState(null);
 
   const getHistoricData = useCallback(async () => {
     const historicApiKey = process.env.REACT_APP_HISTORIC_WEATHER_API;
     if (birthdayInfo) {
-        const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${birthdayInfo.birthdayLocation}/${birthdayInfo.birthdayDate}/${birthdayInfo.birthdayDate}?unitGroup=metric&include=days&key=${historicApiKey}&contentType=json`;
+      const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${birthdayInfo.birthdayLocation}/${birthdayInfo.birthdayDate}/${birthdayInfo.birthdayDate}?unitGroup=metric&include=days&key=${historicApiKey}&contentType=json`;
       try {
         const response = await fetch(url);
         const data = await response.json();
-        setBirthdayInfo(null)
+        // setBirthdayInfo(null);
         setBirthdayWeatherData(data);
-        
       } catch (err) {
         console.log(err.message);
       }
@@ -30,9 +28,31 @@ const HistoricWeather = () => {
 
   return (
     <div className={css.historicWeatherOuterContainer}>
-      <p>How was the weather like in your birthday</p>
-      <CalendarInput setBirthdayInfo={setBirthdayInfo} />
-      {birthdayWeatherData && <DisplayBirthWeather birthdayWeatherData={birthdayWeatherData}/>}
+      {!birthdayInfo && (
+        <div className={css.callToActionInput}>
+          <p>How was the weather like in your birthday</p>
+        </div>
+      )}
+      {!birthdayInfo && <CalendarInput setBirthdayInfo={setBirthdayInfo} />}
+      {birthdayWeatherData && (
+        <DisplayBirthWeather birthdayWeatherData={birthdayWeatherData} />
+      )}
+
+      {!birthdayInfo && (
+        <div className={css.articleInfo}>
+          <p>
+            Humans constantly experience and react to ambient temperature.
+            Because temperature varies markedly across the world, it is
+            conceivable that temperature shapes the fundamental dimensions of
+            personality by affecting the habitual behaviours that underlie
+            personality traits.
+          </p>
+          <p>
+            Temperature may shape personality directly by influencing individual
+            behaviours (for example, exploring outdoors versus staying indoors)
+          </p>
+        </div>
+      )}
     </div>
   );
 };
